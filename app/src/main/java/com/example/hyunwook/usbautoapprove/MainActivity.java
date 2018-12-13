a72c869ef14e1428653369cf1a13dddc308700ae
hyunwook <c004245@naver.com>
2018-12-14 00:48:11 +0800

    Usb Permission SendBroadcast * VendorId, ProductId, DeviceClass, DeviceSubClass

--- app/src/main/java/com/example/hyunwook/usbautoapprove/MainActivity.java
+++ app/src/main/java/com/example/hyunwook/usbautoapprove/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hyunwook.usbautoapprove;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<String> mReaderAdapter; //Usb Device Adapter.
 
     private UsbManager mManager;
+
+    static final String MY_APP_PACKAGENAME = "com.example.hyunwook.usbautoapprove";
+    static final String ACTION_USB_PERMISSION_APP =  "ACTION_USB_PERMISSION_ISSUER";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,5 +45,38 @@ public class MainActivity extends AppCompatActivity {
             //Init Usb Device.
             UsbInit();
         }
+    }
+
+    //Usb Device Init Method
+    private void UsbInit() {
+        UsbInitPermission(1);
+
+        //Main Thread sleep.
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Usb Permission SendBroadcast
+    private void UsbInitPermission(int id) {
+        Intent intent = new Intent();
+        intent.setAction(ACTION_USB_PERMISSION_APP);
+        intent.putExtra("packageName", MY_APP_PACKAGENAME); //current app packageName.
+
+        //device_filter.xml
+        intent.putExtra("vendorId", 1839); //My USB Card Reader Device Vendor Id 1839
+        switch (id) {
+            case 1:
+                intent.putExtra("productId", 8761); //My USB Card Reader Device Product Id 8761
+                intent.putExtra("deviceClass", 0);
+                intent.putExtra("deviceSubclass", 0);
+                break;
+        }
+
+        sendBroadcast(intent);
+
+
     }
 }
